b3c2c47e9f6450c974f973dd2348a1b847cb34f9
ffino <ffino1506@gmail.com>
2016-12-11 17:53:12 +0100

    end of section2

--- src/main/java/org/teachingkidsprogramming/section02methods/PyramidsOfGiza.java
+++ src/main/java/org/teachingkidsprogramming/section02methods/PyramidsOfGiza.java
@@ -9,21 +9,31 @@ public class PyramidsOfGiza
   {
     setUpPyramidLand();
     //  Turn the tortoise 90 degrees to the left 
+    Tortoise.turn(-90);
     //  Move the tortoise 220 pixels
+    Tortoise.move(220);
     //  Turn the tortoise 135 degrees to the right 
-    //  Move the tortoise 100 pixels
-    //  Turn the tortoise 90 degrees to the right
-    //  Move the tortoise 100 pixels
+    Tortoise.turn(135);
+    drawPyramid();
     //  Turn the tortoise 90 degrees to the left 
-    //  Move the tortoise 100 pixels
-    //  Turn the tortoise 90 degrees to the right 
-    //  Move the tortoise 100 pixels
+    Tortoise.turn(-90);
+    drawPyramid();
     //  Turn the tortoise 90 degrees to the left 
-    //  Move the tortoise 100 pixels
-    //  Turn the tortoise 90 degrees to the right 
-    //  Move the tortoise 100 pixels
+    Tortoise.turn(-90);
+    drawPyramid();
     //  Turn the tortoise 135 degrees to the right 
+    Tortoise.turn(135);
     //  Move the tortoise 210 pixels
+    Tortoise.move(210);
+  }
+  private static void drawPyramid()
+  {
+    //  Move the tortoise 100 pixels
+    Tortoise.move(100);
+    //  Turn the tortoise 90 degrees to the right
+    Tortoise.turn(90);
+    //  Move the tortoise 100 pixels
+    Tortoise.move(100);
   }
   private static void setUpPyramidLand()
   {
