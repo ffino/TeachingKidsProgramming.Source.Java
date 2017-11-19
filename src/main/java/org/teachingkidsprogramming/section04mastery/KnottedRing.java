0bad4fd41ce31a8f122eab976ea518fd11692191
ffino <ffino1506@gmail.com>
2017-11-19 17:09:09 +0100

    KnottedRing.java FINISHED

--- src/main/java/org/teachingkidsprogramming/section04mastery/KnottedRing.java
+++ src/main/java/org/teachingkidsprogramming/section04mastery/KnottedRing.java
@@ -10,19 +10,35 @@ public class KnottedRing
   {
     Tortoise.show();
     //      Make the tortoise move as fast as possible --#4
+    Tortoise.setSpeed(10);;
     createColorPalette();
     //          Do the following 30 times --#10.1
+    for (int i = 0; i < 30; i++)
+    {
     //              Change the pen color of the line the tortoise draws to the next color from the color wheel --#5
+      Tortoise.setPenColor(ColorWheel.getNextColor());
     //              drawOctagonWithOverlap (recipe below) --#8.0
-    //              ------------- Recipe for drawOctagonWithOverlap --#7.1
-    //                  Do the following 8 + 1 times --#3.1
-    //                        Move the tortoise 110 pixels --#1
-    //                        Turn the tortoise 1/8th of 360 degrees to the right --#2
-    //                  End Repeat --#3.2
-    //              ------------- End of drawOctagonWithOverlap recipe --#7.2
+    drawOctagonWithOverLap();
     //       Turn the tortoise 1/30th of 360 degrees to the right --#9
+    Tortoise.turn(360 / 30);
     //       Turn the tortoise 5 more degrees to the right --#11
+    Tortoise.turn(5);
     //       End Repeat --#10.2
+    }
+  }
+  private static void drawOctagonWithOverLap()
+  {
+    //              ------------- Recipe for drawOctagonWithOverlap --#7.1
+  //                  Do the following 8 + 1 times --#3.1
+    for (int y = 0; y < 9; y++)
+    {
+  //                        Move the tortoise 110 pixels --#1
+      Tortoise.move(110);
+  //                        Turn the tortoise 1/8th of 360 degrees to the right --#2
+      Tortoise.turn(360.0 / 8);
+  //                  End Repeat --#3.2
+    }
+  //              ------------- End of drawOctagonWithOverlap recipe --#7.2
   }
   private static void createColorPalette()
   {
